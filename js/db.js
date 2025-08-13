f625922a65b6415b9f21a6741392ca36bbc3bc16
Satriya Adi Firmansyah <satriyaadifirmansyah718@gmail.com>
2025-08-13 16:27:50 +0700

    Update

--- js/db.js
+++ js/db.js
@@ -1,12 +1,12 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false
+  connectionString: 'postgresql://postgres:xabVzSgZVvaxFEpIsSnoepZNDkoNBsQu@shuttle.proxy.rlwy.net:25968/railway',
+  ssl: { rejectUnauthorized: false }
 });
+
+module.exports = pool;
+  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false
+;
 
 module.exports = pool;
